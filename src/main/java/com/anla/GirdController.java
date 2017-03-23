7a3f5101d0ea456d347d12a68d56309e3fafdabe
zanjs <root@zanjs.com>
2017-03-23 18:22:20 +0800

    Winners do what losers don't want to do :+1:

--- src/main/java/com/anla/GirdController.java
+++ src/main/java/com/anla/GirdController.java
@@ -1,8 +1,7 @@
 package com.anla;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,8 +13,71 @@ public class GirdController {
     @Autowired
     private GirlRepository girlRepository;
 
+    /**
+     * 查询所有女生列表
+     * @return
+     */
     @GetMapping(value = "/girls")
     public List<Girl> girlList(){
         return girlRepository.findAll();
+    }
+
+    /**
+     * 添加一个女生
+     * @param cupSize
+     * @param age
+     * @return
+     */
+    @PostMapping(value = "/girls")
+    public Girl add(@RequestParam("cupSize") String cupSize,
+                      @RequestParam("age") Integer age){
+        Girl girl = new Girl();
+        girl.setCupSize(cupSize);
+        girl.setAge(age);
+        return girlRepository.save(girl);
+    }
+
+    /**
+     * 查询谋个女生
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "/girls/{id}")
+    public Girl show(@PathVariable("id") Integer id){
+        return girlRepository.findOne(id);
+    }
+
+    /**
+     * 更新一个女生
+     * @param id
+     * @param cupSize
+     * @param age
+     * @return
+     */
+    @PutMapping(value = "/girls/{id}")
+    public Girl update(@PathVariable("id") Integer id,
+                      @RequestParam("cupSize") String cupSize,
+                      @RequestParam("age") Integer age) {
+
+        Girl girl = new Girl();
+        girl.setId(id);
+        girl.setCupSize(cupSize);
+        girl.setAge(age);
+
+        return girlRepository.save(girl);
+    }
+
+    /**
+     * 删除一个女生
+     * @param id
+     */
+    @DeleteMapping(value = "/girls/{id}")
+    public void del(@PathVariable("id") Integer id){
+        girlRepository.delete(id);
+    }
+
+    @GetMapping(value = "/girls/age/{age}")
+    public List<Girl> showAge(@PathVariable("age") Integer age){
+        return girlRepository.findByAge(age);
     }
 }
--- src/main/java/com/anla/GirlRepository.java
+++ src/main/java/com/anla/GirlRepository.java
@@ -2,8 +2,16 @@ package com.anla;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Created by Anl on 2017/3/23.
  */
 public interface GirlRepository extends JpaRepository<Girl, Integer> {
+    /**
+     * 通过年龄查查
+     * @param age
+     * @return
+     */
+    public List<Girl> findByAge(Integer age);
 }
